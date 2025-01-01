cdb8c98bc3230b6504ce791b392bb6d744e8b893
Md Al amin <76806914+mhalamin978@users.noreply.github.com>
2025-01-01 22:54:47 +0600

    Add files via upload

---
+++ 2temcovert.py
@@ -0,0 +1,8 @@
+
+#Write a program that will convert celsius value to fahrenheit
+
+celcius =float(input("Enter the temp in celcius: "))
+
+fahrenheit = celcius*9/5 +32
+
+print(fahrenheit)
---
+++ 3swapnum.py
@@ -0,0 +1,9 @@
+
+
+#User will input (2numbers).Write a program to swap the numbers
+
+a=float(input("Enter num a:"))
+b=float(input("Enter num b:"))
+
+print("your num a is :",b)
+print("your num b is :",a)
